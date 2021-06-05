428aa90bf6db8bf89a19963a8f8b118e28e99a5a
Luke Krikorian <lukekrikorian@gmail.com>
2021-06-04 21:40:13 -0400

    location hash linking

--- static/photos.js
+++ static/photos.js
@@ -1,15 +1,25 @@
+var less = ["ArrowUp", "ArrowLeft", "k", "h"];
+var more = ["ArrowDown", "ArrowRight", "j", "l"];
+
 var container = document.getElementById("container")
 var max = document.getElementsByClassName("thumb").length - 1
 var cursor = -1
  
 function toggle(i) {
+	if (typeof i !== "number" || isNaN(i) || i < 0 || i > max) {
+		return
+	}
+
 	cursor = i	
 	console.log("going to", i)
 	var url = "static/photos/" + i + ".png";
+
+	// Remove currently shown image, if any
 	var show = document.querySelector(".show")
 	if (show !== null && show.id !== url) {
 		show.classList.remove("show")
 	}
+
 	var img = document.getElementById(url);
 	if (img === null) {
 		img = document.createElement("img");
@@ -19,23 +29,33 @@ function toggle(i) {
 		container.appendChild(img)
 	} else {
 		img.classList.toggle("show")
+		if (!img.classList.contains("show") && history.replaceState) {
+			history.replaceState(null, null, " ")
+			return
+		}
+	}
+
+	if (history.replaceState) {
+		history.replaceState(null, "Picture #" + i, "#" + i)
 	}
 }
 
-var less = ["ArrowUp", "ArrowLeft", "k", "h"];
-var more = ["ArrowDown", "ArrowRight", "j", "l"];
 
 function keylisten(e) {
 	var next = cursor
 	if (less.includes(e.key)) {
-		next = Math.max(cursor - 1, 0)
+		next -= 1
 	} else if (more.includes(e.key)) {
-		next = Math.min(cursor + 1, max)
+		next += 1 
 	} 
 
-	if (next !== cursor) {
-		toggle(next)
-	}
+	toggle(next)
+}
+
+var hash = window.location.hash
+if (hash.length > 0) {
+	var id = parseInt(hash.substring(1))
+	toggle(id)
 }
 
 window.addEventListener("keydown", keylisten, true)
